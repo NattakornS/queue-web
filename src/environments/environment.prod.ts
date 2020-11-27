b0bd4b97b1e84bdde2960e1a24225137203c0d2d
Nattakorn Sanpabopit <ntks@pop-os.localdomain>
2020-11-27 22:09:54 +0700

    add environment example

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: '/api/v1'
+  apiUrl: '/api/v1',
+  rightApiUrl: 'http://localhost:5001',
+  rightUser: 'xx',
+  rightPassword: 'xx',
+  rightAdmin: 'xx'
 };
